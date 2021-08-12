0ea52bb9f3931e05a219a400b812835bf51155e4
maryletteroa <maryletteroa@gmail.com>
2021-08-12 21:56:10 +0800

    update requirements add gunicorn

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import requests
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("APP_SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/movies.db"
+app.config["SECRET_KEY"] = os.environ.get("APP_SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 Bootstrap(app)
